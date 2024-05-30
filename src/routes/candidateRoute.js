533c410ed416d056a864b6ebc765249b1b6a489d
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2024-05-30 22:26:48 +0530

    add voting route

--- src/routes/candidateRoute.js
+++ src/routes/candidateRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Candidate = require('../../database/models/candidate');
 const { jwtAuthMiddleware } = require('../controller/jwt');
 const checkIsAdmin = require('../controller/adminRole'); // Corrected function name
+const User = require('../../database/models/user');
 
 // POST route to add a candidate
 router.post('/', jwtAuthMiddleware, async (req, res) => {
@@ -69,7 +70,31 @@ router.delete('/:candidateID', jwtAuthMiddleware, async (req, res) => {
 
 // ------------------ lets start voting -----------------
 router.post("/vote/:candidateID", jwtAuthMiddleware, async (req, res) => {
-  
+    candidateID = req.params.candidateID;
+    userId = req.user.id;
+    try {
+        const candidate = await Candidate.findById(candidateID);
+        if (!candidate) {
+            return res.status(404).json({ message: "candidate not found" });
+        }
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "user not found" });
+        }
+        if (user.isVoted) {
+            res.status(400).json({ message: "you have alredy voted" });
+        }
+        if (user.role == "admin") {
+            res.status(403).json({ message: "admin is not allowed for vote" });
+        }
+
+        // ---- update candidate ----
+        Candidate.votes.push({ user: userId });
+        Candidate.voteCount++;
+        await Candidate.save();
+    } catch (error) {
+
+    }
 });
 
 module.exports = router; 
