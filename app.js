aef3708706ab414403288eb3e85436ed4cff8849
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2024-05-30 22:34:10 +0530

    add routing code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const BodyParser = require("body-parser");
 const app = express();
 const color = require("colors");
 
-require("dotenv").config({path: "./config/.env"});
+require("dotenv").config({ path: "./config/.env" });
 require("./database/connection/mongodb");
 const port = process.env.PORT || 9000;
 
@@ -13,11 +13,12 @@ app.use(BodyParser.json());
 // -------------- require routes -----------------
 const userRoutes = require("./src/routes/userRoutes");
 const candidateRoute = require("./src/routes/candidateRoute")
+const votingRoute = require("./src/routes/votingRoute")
 
 app.use("/user", userRoutes);
 app.use("/candidate", candidateRoute);
+app.use("/voting", votingRoute);
 
-
-app.listen(port, ()=>{
+app.listen(port, () => {
      console.log(`Server Listening On port ${port}`.bgMagenta.black);
 });
